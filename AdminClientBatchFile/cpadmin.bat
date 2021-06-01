cb6456e2c867b1215b19ca8fb54465203573c22d
Pooja Nikumbh <Pooja.Nikumbh@sterlingts.com>
2021-06-01 16:16:55 +0530

    Final Upload

---
+++ AdminClientBatchFile/cpadmin.bat
@@ -0,0 +1,4 @@
+@echo off 
+Echo "Please Wait Checking and Updating AdminClient"
+call robocopy "\\pbtpfil01\AdminClient_Prod" "c:\Admin Client\Client" /mir /R:1 /W:5 >null
+Echo "copied at path : c:\Admin Client\Client"
